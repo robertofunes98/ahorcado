8253d70eae20a5a622a312de87a344213a2eb1d7
vanegas <mariocanario98@gmail.com>
2018-02-28 23:38:06 -0600

    funciones Single y de conexion finalizadas (falta test)

--- engine/conexDB.php
+++ engine/conexDB.php
@@ -30,13 +30,36 @@
             }
         }
 
-        public function ingresarDatos($tabla, $datos) {
-            $sql = "INSERT INTO " . $tabla . "() VALUES (" . $datos . ")";
+        public function ingresarDatos($tabla, $datos, $campos) {
+            $sql = "INSERT INTO " . $tabla . "(". $campos . ") VALUES (" . $datos . ")";
 
             if ($this->conexion->query($sql) === TRUE) {
-                echo "<script type='text/javascript'>alert('Palabra Ingresada Correctamente');</script>";
+                echo "<script type='text/javascript'>alert('Datos Ingresados Correctamente');</script>";
             } else {
                 echo "Error: " . $this->conexion->error;
+            }
+        }
+
+        public function consultaGeneral($tabla) {
+            $sql = "SELECT * FROM " . $tabla;
+            $resul = $this->conexion->query($sql);
+
+            if ($result->num_rows > 0) {
+                $datos = $result->fetch_assoc();
+                return $datos;
+            } else {
+                echo "<script type='text/javascript'>alert('No hay datos');</script>";
+            }
+        }
+
+        public function consultaPersonalizada($sql) {
+            $resul = $this->conexion->query($sql);
+
+            if ($result->num_rows > 0) {
+                $datos = $result->fetch_assoc();
+                return $datos;
+            } else {
+                echo "<script type='text/javascript'>alert('No hay datos');</script>";
             }
         }
 
