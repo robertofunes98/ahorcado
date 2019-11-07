d637d6d4815a0699b6055db07a9fe3b05ab612f7
robertofunes98 <robertofunes98@gmail.com>
2019-11-06 19:17:01 -0600

    prueba de integraicon continua 3

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     include_once("engine/engine.php");
     @session_start();
-    echo "esta es la integracion continua";
+    echo "esta es la integracion continua 2";
 ?>
 
 <!DOCTYPE html>
