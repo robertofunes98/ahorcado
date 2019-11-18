788553ba668660a1e1e4f4f5c971c3cbc25a40ab
robertofunes98 <robertofunes98@gmail.com>
2019-11-17 23:11:42 -0600

    tutorial para ci/cd con aplicacion de google

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <center><h1><p>Ahorcado Virtual</p></h1></center>
             <center><h2><p>Bienvenido <font color='#01b438'><?php echo $_SESSION['usuario']; ?></font></p></h2></center>
 
-            <center><h3><p>Bienvenido al tutorial <br> Ahora usando integracion continua de google <font color='#01b438'>B). </font></p></h3></center>
+            <center><h3><p>Bienvenido al tutorial de integracion e implementacion continua <br> Ahora usando integracion continua de google <font color='#01b438'>B). </font></p></h3></center>
 
             <center><h3><p>ITCA-FEPADE 2019</p></h3></center>
 
